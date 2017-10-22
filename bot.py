508b7c2a9df73a8f49bc2a46b840d67847645151
QuantumBits <classicrockjunkie@gmail.com>
2017-10-22 00:02:02 -0700

    FINAL FORM ACHIEVED - Full reddit generality!

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ with open('.secrets') as f:
 
     secrets = yaml.load(secret_file)
 
+    print(secrets['keywords'])
+
     client = discord.Client()
     reddit = praw.Reddit(client_id=secrets['client_id'], client_secret=secrets['client_secret'], user_agent=secrets['user_agent'])
 
@@ -24,35 +26,37 @@ with open('.secrets') as f:
     async def on_message(message):
         message_string = message.content
 
-        if 'prequel' in message_string and 'meme' in message_string:
+        for keyword in secrets['keywords']:
+            if not re.search(keyword, message_string, re.IGNORECASE):
+                return
 
-            hot_memes = reddit.subreddit(secrets['subreddit']).hot()
-            hot_links = [meme.shortlink for meme in hot_memes]
-            hot_link_not_good = True
+        hot_memes = reddit.subreddit(secrets['subreddit']).hot()
+        hot_links = [meme.shortlink for meme in hot_memes]
+        hot_link_not_good = True
 
-            while hot_link_not_good:
-                try:
-                    hot_link = random.choice(hot_links)
-                    json_link = "https://www.reddit.com/" + re.search(r"^https:\/\/redd.it\/(.*)$",hot_link).group(1) + ".json"
-                    res = requests.get(json_link)
-                    res_text = res.text
-                    res_data = json.loads(res_text)
-                    img_link = res_data[0]['data']['children'][0]['data']['url']
-                    img_title = res_data[0]['data']['children'][0]['data']['title']
-                    hot_link_not_good = False
-                except Exception:
-                    hot_link_not_good = True
+        while hot_link_not_good:
+            try:
+                hot_link = random.choice(hot_links)
+                json_link = "https://www.reddit.com/" + re.search(r"^https:\/\/redd.it\/(.*)$",hot_link).group(1) + ".json"
+                res = requests.get(json_link)
+                res_text = res.text
+                res_data = json.loads(res_text)
+                img_link = res_data[0]['data']['children'][0]['data']['url']
+                img_title = res_data[0]['data']['children'][0]['data']['title']
+                hot_link_not_good = False
+            except Exception:
+                hot_link_not_good = True
 
-            
-            print(hot_link)
-            print(img_title)
-            print('------------')
+        
+        print(hot_link)
+        print(img_title)
+        print('------------')
 
-            em = discord.Embed(title=img_title)
-            em.set_author(name=secrets['post_title'])
-            em.set_image(url=img_link)
-            em.url = hot_link
+        em = discord.Embed(title=img_title)
+        em.set_author(name=secrets['post_title'])
+        em.set_image(url=img_link)
+        em.url = hot_link
 
-            await client.send_message(message.channel, embed=em)
+        await client.send_message(message.channel, embed=em)
 
     client.run(secrets['bot_token'])
